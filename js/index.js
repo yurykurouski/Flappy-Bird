bd984a57919aae0168b636f27bdc4cb4d4367ffc
Yury <ymorpheus@gmail.com>
2021-02-12 12:40:41 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const audioCoin = document.getElementById('audioCoin');
-const audioWing = document.getElementById('audioWing')
+const audioWing = document.getElementById('audioWing');
 
 const background = new Image();
 const bird = new Image();
@@ -15,26 +15,23 @@ pipeUp.src = './img/flappy_bird_pipeUp.png';
 pipeBottom.src = './img/flappy_bird_pipeBottom.png';
 scoreBg.src = './img/score-cropped.png';
 
-const gravity = 1.5;
+const gravity = 2;
 const gap = 85;
 
 const birdX = 0;
 let birdY = canvas.height / 2;
 
-const birdWidth = 36;
-const birdHeight = 26;
-
 context.font = 'bold 32px serif';
-context.textAlign = 'center'
+context.textAlign = 'center';;
 context.fillStyle = 'Black'
 context.fillText('Press Enter to start', canvas.width / 2, scoreBg.height / 2 + 5);
 
 document.addEventListener('keydown', (event) => {
-  birdY = birdY <= 26 ? 0 : birdY - 50;
-  audioWing.play();
   if (event.keyCode === 13) {
-    game()
+    game();
   }
+  birdY = birdY <= 26 ? 0 : birdY - 80;
+  audioWing.play();
 });
 
 let cycle = 0;
@@ -53,13 +50,12 @@ function game() {
     context.drawImage(background, bgX, 0);
     bgX--;
     context.drawImage(background, bgX + background.width, 0);
-
-    context.drawImage(bird, cycle * birdWidth, 0, birdWidth, birdHeight, birdX, birdY, birdWidth, birdHeight);
+    context.drawImage(bird, cycle * bird.width / 3, 0, bird.width / 3, bird.height, birdX, birdY, bird.width / 3, bird.height);
 
     if (Math.abs(bgX) === background.width) {
-      bgX = 0
+      bgX = 0;
     }
-  }, 48);
+  }, 36);
 
   enviroment = setInterval(() => {
 
@@ -98,12 +94,12 @@ function game() {
     context.drawImage(scoreBg, canvas.width / 2 - 55, -5);
 
     context.font = 'bold 32px serif';
-    context.textAlign = 'center'
-    context.fillStyle = 'white'
+    context.textAlign = 'center';
+    context.fillStyle = 'white';
     context.fillText(score, canvas.width / 2, scoreBg.height / 2 + 5);
 
     // birdY = birdY >= canvas.height - 26 ? canvas.height / 2 : birdY + gravity;
-    birdY = birdY + gravity
+    birdY = birdY + gravity;
 
   }, 12);
 }
@@ -114,8 +110,8 @@ function gameOver() {
   setTimeout(() => location.reload(), 5000);
 
   context.font = 'bold 32px serif';
-  context.textAlign = 'center'
-  context.fillStyle = 'white'
+  context.textAlign = 'center';
+  context.fillStyle = 'white';
   context.fillText('You loose.', canvas.width / 2, canvas.height / 2);
   context.fillText('Restart in 5 seconds.', canvas.width / 2, canvas.height / 2 + 32);
 }
